728bfcb9368993f658dc3471aa4c86af4a0336b3
Camron Moore <camronbmoore@gmail.com>
2016-03-09 00:45:43 +0000

    Added integration tests for add user and article

--- test/integration/create_user_test.rb
+++ test/integration/create_user_test.rb
@@ -1,19 +1,16 @@
 require 'test_helper'
 
 class CreateUserTest < ActionDispatch::IntegrationTest
-  
-  def setup
-    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
-  end
+
   
   test "get sign up form and create user" do
-    get user_path
+    get signup_path
     assert_template 'users/new'
-    assert_difference 'Category.count', 1 do
-      post_via_redirect user_path, user: {username: "john", email: "john@example.com", password: "password", admin: true}
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {username: "john", email: "john@example.com", password: "password", admin: false}
     end
-    assert_template 'user/index'
-    assert_match "john", response.body
+    assert_template 'users/show'
+    assert_match 'john', response.body
   end
   
 end
